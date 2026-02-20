6adc8f07c0996eff0e517f42407606fde679ec96
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-20 14:43:10 +0700

    Update file home_page.dart , rubah pada Tampilan text counter

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -51,6 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const Text('You have pushed the button this many times:'),
+                Text(
+                  value.angka.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
         
               ],
             ),

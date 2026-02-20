b7bfedb87a575ad6468a2529141a8d77a3ef71d1
IlhamMaulana13 <mhmmad13ilhammlna@gmail.com>
2026-02-20 14:35:02 +0700

    Update pada main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:s/counter_model.dart';
 import 'package:s/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
